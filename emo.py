15b35e419c81e72451b4cc9dd767c6ef26fdabf1
Jack <107597797+Jack1Hayes@users.noreply.github.com>
2022-11-02 18:44:04 +0000

    Update emo.py

--- emo.py
+++ emo.py
@@ -18,8 +18,8 @@ from keras.utils import to_categorical
 
 
 
-icml_faces = pd.read_csv(r'C:\Users\Hayes\OneDrive\Documents\challenges-in-representation-learning-facial-expression-recognition-challenge\icml_face_data.csv')
-#icml_faces.head()
+icml_faces = pd.read_csv('imported data')
+icml_faces.head()
 
 X_train = []
 y_train = []
@@ -27,6 +27,7 @@ X_test = []
 y_test = []
 for index, row in icml_faces.iterrows():
     k = row[" pixels"].split(" ")
+    k =  [int(x) for x in k]
     if row[' Usage'] == 'Training':
         X_train.append(np.array(k))
         y_train.append(row['emotion'])
@@ -44,8 +45,8 @@ X_test = X_test.reshape(X_test.shape[0], 48, 48, 1)
 
 #input_labels = np.array(list(map(int, icml_faces['emotion'])))
 
-y_train = to_categorical(y_train, 7)
-y_test = to_categorical(y_test, 7)
+y_train= to_categorical(y_train, num_classes=7)
+y_test = to_categorical(y_test, num_classes=7)
 
 model = Sequential()
 model.add(ZeroPadding2D((1,1),input_shape=(48,48,1)))
@@ -60,31 +61,6 @@ model.add(ZeroPadding2D((1,1)))
 model.add(Convolution2D(128, 3, 3, activation='relu'))
 model.add(MaxPooling2D((2,2), strides=(2,2), padding='same'))
 
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(256, 3, 3, activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(256, 3, 3, activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(256, 3, 3, activation='relu'))
-model.add(MaxPooling2D((2,2), strides=(2,2), padding='same'))
-
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, 3, 3, activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, 3, 3, activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, 3, 3, activation='relu'))
-model.add(MaxPooling2D((2,2), strides=(2,2), padding='same'))
-
-
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, 3, 3, activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, 3, 3, activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, 3, 3, activation='relu'))
-model.add(MaxPooling2D((2,2), strides=(2,2), padding='same'))
-
 model.add(Flatten())
 model.add(Dense(4096, activation='relu'))
 model.add(Dropout(0.5))
@@ -92,11 +68,13 @@ model.add(Dense(4096, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(7, activation='softmax'))
 
-model.compile(optimizer='Adam',loss='categorical_crossentropy',metrics=['accuracy'])
+model.compile(optimizer='sgd',loss='categorical_crossentropy',metrics=['accuracy'])
 
-from sklearn.preprocessing import LabelEncoder
-lb = LabelEncoder()
-y_test = lb.fit_transform(y_test)
-y_train = lb.fit_transform(y_train)
 
-model.fit(X_train, y_train, batch_size=32, epochs=30, verbose=1, validation_data=(X_test, y_test))
+model.fit(X_train, y_train, batch_size=32, epochs=100, verbose=1, validation_data=(X_test, y_test))
+
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+  json_file.write(model_json)
+  model.save_weights("model.h5")
+  print("Saved model to disk")
